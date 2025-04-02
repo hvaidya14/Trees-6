0157556fbae0d1de390239f4e3fe4da4335bf732
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-04-01 20:35:52 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -123,3 +123,45 @@ class Solution {
         
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum=0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return -1;
+        }
+        dfs(root, low, high);
+        return sum;
+    }
+
+    private void dfs(TreeNode root, int low, int high) {
+        if(root == null) {
+            return;
+        }
+        if(root.val >= low && root.val <= high) {
+            sum = sum + root.val;
+            dfs(root.left, low, high);
+            dfs(root.right, low, high);
+        } else if(root.left != null && root.left.val >= low) {
+            dfs(root.left, low, high);
+        } else if(root.right != null && root.right.val <= high) {
+            dfs(root.right, low, high);
+        }
+
+    }
+}
